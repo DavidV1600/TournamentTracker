8af469529e07e8c463c93c77e7f0a21413876285
DavidV1600 <david.voinescu@saguna.ro>
2023-08-04 15:45:21 +0300

    Add project files.

---
+++ TrackerLibrary/Matchup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class Matchup
+    {
+        public List<MatchupEntry> Entries { get; set; }
+
+        public Team Winner { get; set; }
+
+        public int MatchupRound { get; set; }
+    }
+
+}
---
+++ TrackerLibrary/Tournament.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class Tournament
+    {
+        public string TournamentName { get; set; }
+
+        public decimal EntryFee { get; set; }
+
+        public List<Team> EnteredTeams { get; set; }=new List<Team>();
+
+        public List<Prize> Prizes { get; set; }= new List<Prize>();
+
+        public List<List<Matchup>> Rounds { get; set; }=new List<List<Matchup>>();
+    }
+}
